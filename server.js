148551c6265a8d6e1a3dd23f261890802ef18296
eamahma <eamahma@gmail.com>
2021-08-19 13:44:04 -0400

    formatted view all tables

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const inquirer = require('inquirer');
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+const sequelize = require('./config/connection');
+const Employee = require('./models/Employee');
+
+const app = express();
+app.use(express.json());
+
+
+
 
 // Connect to database
 const db = mysql.createConnection(
@@ -16,28 +24,44 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
-async function viewAllEmployees() {
+async function viewAllEmployees(req, res) {
+  // SQL Query
+  let query = `SELECT employee.id AS ID, employee.first_Name AS 'First Name', employee.last_Name AS 'Last Name', title AS 'Title', name AS 'Department', salary AS 'Salary', GROUP_CONCAT(DISTINCT manager.first_Name,' ', manager.last_Name) AS 'Manager'
+  FROM employees employee
+  JOIN roles ON employee.role_id = roles.id
+  JOIN departments ON roles.department_id = departments.id
+  LEFT JOIN employees manager ON employee.manager_id = manager.id
+  GROUP BY employee.id
+  ORDER BY employee.id ASC`;
+  
   // Query database
-//  db.query('SELECT * FROM employees', function (err, results) {
-    db.query('SELECT employees.first_name, employees.last_name, roles.title AS title, roles.salary AS salary FROM employees LEFT JOIN roles ON employees.role_id=roles.id', function (err, results) {
-      console.table(results);
-    init();
-  });
+  db.query(query, function (err, results) {
+  console.table(results);
+  init();
+});
 }
 
 async function viewAllRoles() {
-  // Query database
-  db.query('SELECT * FROM roles', function (err, results) {
+  // SQL Query
+  let query =`SELECT title AS 'Title', name AS 'Department',  salary AS 'Salary'
+  FROM roles
+  JOIN departments ON roles.department_id=departments.id`;
+  //Query database
+  db.query(query, function (err, results) {
     console.table(results);
     init();
   });
 }
 
 async function viewAllDepartments() {
+  // SQL Query
+  let query =`SELECT name AS 'Department', id AS 'Department ID' 
+  FROM departments`;
   // Query database
-  db.query('SELECT * FROM departments', function (err, results) {
-    console.table(results);
-    init();
+  //db.query('SELECT * FROM departments', function (err, results) {
+  db.query(query, function (err, results) {
+  console.table(results);
+  init();
   });
 }
 
